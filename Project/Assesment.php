44c4d50949d536c78e18d0621c3fe1a900037096
Strauss-hype <55983201+Strauss-hype@users.noreply.github.com>
2021-01-27 19:07:31 +0200

    Add files via upload

---
+++ Project/Assesment.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+<title>Assesments</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="School.css" rel="stylesheet" type="text/css">
+<style>
+img {
+  border-radius: 8px;
+}
+body {
+  background-image: url('https://images.squarespace-cdn.com/content/v1/5644bc92e4b0d1606755bcbe/1519157913418-Q4IP2QLZKQR6P6VKBUBU/ke17ZwdGBToddI8pDm48kFmfxoboNKufWj-55Bgmc-J7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iXS6XmVv7bUJ418E8Yoc1hjuviiiZmrL38w1ymUdqq4JaGeFUxjM-HeS7Oc-SSFcg/Hall+of+Fame+001.jpg?format=2500w');
+  background-repeat: no-repeat;
+}
+</style>
+</head>
+
+<body>
+<header> <img src="https://wallpapercave.com/wp/wp5016555.jpg" class="profile-img">
+  <nav>
+    <ul>
+      <li><a href="Home.php">Home</a></li>
+      <li><a href="Admission.php">Adimssion</a></li>
+      <li><a href="Registration.php">Registration</a></li>
+      <li><a href="Assesment.php">Assesment</a></li>
+      <li><a href="Contact.php">Contact Us</a></li>
+	  <li><a href="Signup.php">Sign Up</a></li>
+	  <li><a href="Login.php">Log In</a></li>
+    </ul>
+  </nav>
+</header>
+<main>
+  <section id="Assesments">
+    <div class="section-inner"> <img src="https://st.depositphotos.com/1518767/2698/i/600/depositphotos_26981099-stock-photo-magnifying-glass-showing-assessment-word.jpg"  class="profile-img" height="150px">
+<center>
+	<h1>View Assesments</h1>
+<body>
+</body>
+ </center> 
+ </section>
+</main>
+<footer> @ Copyright C Heuva JSS, 2021 </footer>
+</body>
+</html>
+
+
+
---
+++ Project/heuva.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 27, 2021 at 05:04 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `heuva`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assesments`
+--
+
+DROP TABLE IF EXISTS `assesments`;
+CREATE TABLE IF NOT EXISTS `assesments` (
+  `Leaner ID` int(11) NOT NULL,
+  `Subject` varchar(100) NOT NULL,
+  `Test Mark` int(11) NOT NULL,
+  ` Test %` int(11) NOT NULL,
+  `Assignement Mark` int(11) NOT NULL,
+  `Assignment %` int(11) NOT NULL,
+  `Average Total` int(11) NOT NULL,
+  PRIMARY KEY (`Leaner ID`),
+  UNIQUE KEY `Subject` (`Subject`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE IF NOT EXISTS `course` (
+  `Learner ID` int(15) NOT NULL,
+  `Subject  Name` varchar(100) NOT NULL,
+  PRIMARY KEY (`Learner ID`),
+  UNIQUE KEY `Subject  Name` (`Subject  Name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `examination result`
+--
+
+DROP TABLE IF EXISTS `examination result`;
+CREATE TABLE IF NOT EXISTS `examination result` (
+  `Learner ID` int(15) NOT NULL,
+  `Subject` varchar(50) NOT NULL,
+  `CA Mark` int(11) NOT NULL,
+  `Exam Mark` int(11) NOT NULL,
+  `Average %` int(11) NOT NULL,
+  `Total Mark` int(11) NOT NULL,
+  PRIMARY KEY (`Learner ID`),
+  UNIQUE KEY `Subject` (`Subject`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `Learner ID` int(15) UNSIGNED NOT NULL,
+  `First Name` varchar(55) NOT NULL,
+  `Last Name` varchar(55) NOT NULL,
+  `Gender` enum('Male','Female') NOT NULL,
+  `Subject` int(15) NOT NULL,
+  PRIMARY KEY (`Learner ID`),
+  UNIQUE KEY `Course Code` (`Subject`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int(15) NOT NULL,
+  `Username` varchar(100) NOT NULL,
+  `Email` text NOT NULL,
+  `user_type` varchar(10) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
